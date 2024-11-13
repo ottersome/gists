8cf3dbd30aa805a3a583ab8e684b5b8e6b0c388b
ottersome <luisgarcia26284@gmail.com>
2024-11-13 13:34:18 +0800

    Adding: Off GPU script for convenience.

--- turn_off_gpu.sh
+++ turn_off_gpu.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env sh
 nvidia-smi --id 0000:64:00.0 --persistence-mode 0
 nvidia-smi drain --pciid 0000:64:00.0 --modify 1
 nvidia-smi --persistence-mode 1
 
-# To Reverse it:
-# nvidia-smi drain --pciid 0000:64:00.0 --modify 0
---
+++ turn_on_gpu.sh
@@ -0,0 +1,1 @@
+nvidia-smi drain --pciid 0000:64:00.0 --modify 0
